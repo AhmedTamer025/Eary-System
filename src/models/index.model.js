a2c5ebdc0c7e6af3a4da8d002338ed40bbea14a2
Ahmed Tamer <125506462+AhmedTamer025@users.noreply.github.com>
2026-03-29 23:36:02 +0200

    Add files via upload

--- src/models/index.model.js
+++ src/models/index.model.js
@@ -1,1 +1,37 @@
+import Sequelize from 'sequelize';
 
+import db from './../../config/database.js';
+import AnswerModel from './schema/answer.model.js';
+// import examAnswerModel from './schema/ea.model.js';
+import ExamModel from './schema/exam.model.js';
+//* importing models *//
+import UserModel from './schema/user.model.js';
+import { createAdminUser } from '../controllers/user.controller.js';
+
+const user = UserModel(db, Sequelize);
+const exam = ExamModel(db, Sequelize);
+const answer = AnswerModel(db, Sequelize);
+// const examAnswer = examAnswerModel(db, Sequelize);
+
+exam.belongsTo(user, {
+  onDelete: 'CASCADE',
+  onUpdate: 'CASCADE',
+});
+user.hasMany(exam);
+
+answer.belongsTo(exam, {
+  onDelete: 'CASCADE',
+  onUpdate: 'CASCADE',
+});
+exam.hasMany(answer);
+
+db.sync({ force: false }).then(() => {
+  console.log('Tables Created!');
+  createAdminUser();
+});
+
+export default {
+  user,
+  exam,
+  answer,
+};
