d113ffa1a0d97c682ac0e615a96b43d2e7181b88
Ahmed Tamer <125506462+AhmedTamer025@users.noreply.github.com>
2026-03-29 23:02:08 +0200

    Add files via upload

--- client/src/guards/admin.guard.js
+++ client/src/guards/admin.guard.js
@@ -2,16 +2,14 @@ import { Navigate, Outlet } from 'react-router-dom';
 
 import { getAuthToken } from '../services/auth.service';
 
-export const UserGuard = () => {
+export const AdminGuard = () => {
   const { token, user } = getAuthToken();
 
-  if (token && !user.isAdmin && user.isActive) {
+  if (token && user.isAdmin) {
     return <Outlet />;
   }
-
-  if (token && user.isAdmin) {
-    return <Navigate to={'/home_admin'} />;
+  if (token && user.isActive) {
+    return <Navigate to={'/home_user'} />;
   }
-
   return <Navigate to={'/'} />;
 };
