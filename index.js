860ff183e5e529fb456398d31a3f6c6d61eb8bcb
Ahmed Tamer <125506462+AhmedTamer025@users.noreply.github.com>
2026-03-29 22:36:01 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import express from 'express';
+import rateLimit from 'express-rate-limit';
+
+// import helmet from 'helmet';
+import config from './config/config.js';
+import routes from './src/routes/index.js';
+
+const app = express();
+const PORT = config.port ?? 3000;
+const HOST = config.host ?? 'localhost';
+
+//** middleware **/
+app.use(cors()); // enables Cross-Origin Resource Sharing
+// app.use(helmet({ crossOriginResourcePolicy: false })); // adds various HTTP headers to secure the app
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static('uploads'));
+
+// rate limiting middleware
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+  message: 'Too many requests from this IP, please try again later',
+});
+app.use(limiter); // apply rate limiting to all routes under '/'
+
+app.use(routes);
+
+app.get('/', (_req, res) => {
+  res.json({
+    message: 'Eary-System web app 💻✌',
+    github: 'https://github.com/Adosh74/Eary-System',
+  });
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Serving running on http://${HOST}:${PORT}`);
+});
